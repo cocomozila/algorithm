f7d9813658103cbaf73c18aed9e2935d60736c9e
josey <joseyun94@naver.com>
2022-02-05 20:17:28 +0900

    백준 18258 - 큐2

---
+++ src/baekjoon/No_18258.java
@@ -0,0 +1,80 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Deque;
+import java.util.ArrayDeque;
+ 
+public class No_18258 {
+ 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		Deque<Integer> q = new ArrayDeque<>();
+		
+		int cnt  = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		while(cnt --> 0) {
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			switch(st.nextToken()) {	
+			
+			case "push":
+				q.offer(Integer.parseInt(st.nextToken()));	
+				break;
+				
+			case "pop" :				
+				Integer item = q.poll();	
+				if(item == null) {
+					sb.append(-1).append('\n');
+				}
+				else {
+					sb.append(item).append('\n');
+				}
+				break;
+				
+			case "size":	
+				sb.append(q.size()).append('\n');
+				break;
+				
+			case "empty":
+				if(q.isEmpty()) {
+					sb.append(1).append('\n');
+				}
+				else {
+					sb.append(0).append('\n');
+				}
+				break;
+				
+			case "front":
+				Integer ite = q.peek();
+				if(ite == null) {
+					sb.append(-1).append('\n');
+				}
+				else {
+					sb.append(ite).append('\n');
+				}
+				break;
+				
+			case "back":
+				Integer it = q.peekLast();	 
+				if(it == null) {
+					sb.append(-1).append('\n');
+				}
+				else {
+					sb.append(it).append('\n');
+				}
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+}
