1097225d9a5241fda8b2478ce8e42467114bb031
josey <joseyun94@naver.com>
2022-02-16 23:24:20 +0900

    프로그래머스 두 개 뽑아서 더하기

---
+++ src/programmers/PickTwoAndAdd.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class PickTwoAndAdd {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public int[] solution(int[] numbers) {
+		
+		HashSet<Integer> set = new HashSet<>();
+		
+		for (int i = 0; i < numbers.length-1; i++) {
+			for (int j = i+1; j < numbers.length; j++) {
+				set.add(numbers[i]+numbers[j]);
+			}
+		}
+		
+		int[] answer = new int[set.size()];
+		
+		Iterator<Integer> iter = set.iterator();
+		
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = iter.next();
+		}		
+		
+		Arrays.sort(answer);		
+		
+        return answer;
+    }
+
+}
