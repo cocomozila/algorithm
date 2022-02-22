6d89fa46708251f58be50089ee77ddd18ea965de
josey <joseyun94@naver.com>
2022-02-23 01:30:40 +0900

    프로그래머스 약수의 합

---
+++ src/programmers/Divisor.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Divisor {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public int solution(int n) {
+        
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+    
+        return answer;
+    }
+
+}
